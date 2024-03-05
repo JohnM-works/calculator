7caa3a08106e8b03d65f0f8a2c26c19d9b47413b
jhin47 <johnmalonzo63@gmail.com>
2024-03-06 03:34:43 +0800

    add a function to remove the 0 when you input the first number

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 let firstNumber = 0;
+let secondNumber = 0;
 
 const numbers = document.querySelectorAll(".number-btn");
+const operators = document.querySelectorAll("operator-btn");
 const numberOutput = document.querySelector(".number-output");
+
+function selectNumber(number) {
+  number = this.textContent;
+
+  if (numberOutput.textContent.length < 10) {
+    if (numberOutput.textContent === "0") {
+      numberOutput.textContent = number;
+    } else {
+      numberOutput.textContent += `${number}`;
+    }
+  }
+}
+
+function operation() {
+  if (operator == "+") {
+    result = firstNumber + secondNumber;
+  } else if (operator == "-") {
+    result = firstNumber - secondNumber;
+  } else if (operator == "*") {
+    result = firstNumber * secondNumber;
+  } else if (operator == "/") {
+    result = firstNumber / secondNumber;
+  }
+}
 
 numbers.forEach((number) => number.addEventListener("click", selectNumber));
 
-function selectNumber(firstNumber) {
-  if (numberOutput.textContent.length < 9) {
-    firstNumber = this.value;
-    numberOutput.textContent += firstNumber;
-    console.log(numberOutput);
-  } else {
-    return;
-  }
-}
+operators.forEach((operator) => operator.addEventListener("click", operation));
